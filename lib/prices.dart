57e11b7320a4d87802846510bcd3a564e7e2ff9a
Fernando Zavalia <24811313+fzavalia@users.noreply.github.com>
2020-09-09 10:49:32 -0300

    Refactor prices

--- lib/prices.dart
+++ lib/prices.dart
@@ -6,6 +6,7 @@ Future<double> getBtcUsdPrice() async {
   final url = 'https://api.coindesk.com/v1/bpi/currentprice.json';
   final response = await http.get(url);
   Map<String, dynamic> json = jsonDecode(response.body);
+
   return json["bpi"]["USD"]["rate_float"];
 }
 
@@ -13,9 +14,11 @@ Future<double> getUsdArsPrice() async {
   final scraper = WebScraper("https://www.dolarhoy.com");
   await scraper.loadWebPage("/cotizaciondolarblue");
   final elements = scraper.getElement("span.pull-right", []);
-  final prices = elements
-      .map((e) => double.parse(e["title"].substring(2).replaceAll(",", ".")))
-      .toList();
+  final prices = elements.map((e) {
+    final price = e["title"].substring(2).replaceAll(",", ".");
+    return double.parse(price);
+  }).toList();
   prices.sort();
+
   return prices[0] + (prices[1] - prices[0]) / 2;
 }
